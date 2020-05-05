b0a8bfa64683016c7fd83d69ae0b84a4968ac7c2
Luke Peña <luketpena@gmail.com>
2020-05-05 14:31:33 -0500

    Cleaned up some redux code and change HTML title

--- src/index.js
+++ src/index.js
@@ -2,22 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-//Redux + Saga
-// import {createStore, applyMiddleware} from 'redux';
-// import {Provider} from 'react-redux';
-// import logger from 'redux-logger';
-// import createSagaMiddleware from 'redux-saga';
-
-// import rootReducer from './redux/reducers';
-// import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
-
-//-----< STORE + MIDDLEWARE >-----\\
-// const sagaMiddlware = createSagaMiddleware();
-// const store = createStore (
-//   rootReducer,
-//   applyMiddleware(sagaMiddlware, logger)
-// )
-// sagaMiddlware.run(rootSaga);
-
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
