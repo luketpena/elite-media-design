56d1b26d28b576efb7ece6bd4e5319fd16abeab1
Luke Peña <luketpena@gmail.com>
2020-05-04 21:13:51 -0500

    Added fixed logo file.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from '../../images/logo.svg';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello, world.</h1>
+      <img src={logo} alt="EMD Logo"/>
     </div>
   );
 }
